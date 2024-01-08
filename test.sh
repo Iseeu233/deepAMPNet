905560e9e7e736e7cd34aa2be298b1e2081b0f9a
Iseeu233 <1353230061@qq.com>
2024-01-08 15:50:59 +0800

    first commit

---
+++ test.sh
@@ -0,0 +1,1 @@
+python test.py --batch_size 32 --test_dataset <your_dataset_for_prediction> --model saved_model/deepAMPNet.pth --prefix out
---
+++ train.sh
@@ -0,0 +1,1 @@
+python train.py --batch_size 64 --epochs 100 --learning_rate 0.0005 --train_dataset <your_train_dataset> --test_dataset <your_test_dataset_test> --dropout 0.5 --num_features 6165 --hidden_dim 512 --net HGPSL --save HGPSL_out
